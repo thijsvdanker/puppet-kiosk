ecbfc2b75943b73535ad6ce70ac25811318120c8
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-04-14 13:36:37 +0200

    added mouse cursor hiding

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,7 +12,7 @@
 #
 
 class kiosk(
-  $packages       = ['xorg','openbox','squid3'],
+  $packages       = ['xorg','openbox','squid3','unclutter'],
   $midoridirs     = ['/home/kiosk/.config','/home/kiosk/.config/midori','/home/kiosk/.config/openbox','/home/kiosk/.local/','/home/kiosk/.local/share/','/home/kiosk/.local/share/midori','/home/kiosk/.local/share/midori/styles'],
 )
 {
