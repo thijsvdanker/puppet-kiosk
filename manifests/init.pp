625fa64665ea7400445c15354050271ff7f77063
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-04-15 11:45:13 +0200

    fixing squid conf

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,7 +17,7 @@ class kiosk(
   $http_port      = "8080",
   $acl_whitelist  = ['.naturalis.nl/nl/het-museum/agenda/','.naturalis.nl/media','.naturalis.nl/static/*'],
   $deny_info      = "http://www.naturalis.nl/nl/het-museum/agenda/",
-  $cache_peer     = ['.naturalis.nl/nl/het-museum/agenda/']
+  $cache_peer     = undef
 )
 {
   include stdlib
