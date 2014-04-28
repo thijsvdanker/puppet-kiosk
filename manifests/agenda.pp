789c486e467b6e72a2aa204f37c04fc6a9d6bc05
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-04-28 16:08:01 +0200

    fixing roles

--- manifests/agenda.pp
+++ manifests/agenda.pp
@@ -2,9 +2,19 @@
 #
 #
 class kiosk::agenda (
-   $homepage                           = "http://www.naturalis.nl/nl/het-museum/agenda/",
+    $homepage                           = "http://www.naturalis.nl/nl/het-museum/agenda/",
     $acl_whitelist                      = ['.naturalis.nl/nl/het-museum/agenda/','.naturalis.nl/media','.naturalis.nl/static/*'],
     $deny_info                          = "http://www.naturalis.nl/nl/het-museum/agenda/",
     $cache_peer                         =  ".naturalis.nl/"
 )
-{}
+{
+  # make whitelist usable with regex
+  $acl_whitelist_real = join($acl_whitelist,'|')
+# if cache_peer not set, use whitelist for caching
+  if ($cache_peer) {
+    $cache_peer_real = $cache_peer
+  }
+  else {
+    $cache_peer_real = $acl_whitelist_real
+  }
+}
