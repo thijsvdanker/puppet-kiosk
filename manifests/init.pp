45cb66dbd9b12783c5d96a50be59eee763d32da4
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-05-09 09:49:43 +0200

    added midori_path variable

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,7 +21,8 @@ class kiosk(
   $homepage                             = "http://www.naturalis.nl/nl/het-museum/agenda/",
   $acl_whitelist                        = [".naturalis.nl/nl/het-museum/agenda/",".naturalis.nl/media",".naturalis.nl/static/*"],
   $deny_info                            = "http://www.naturalis.nl/nl/het-museum/agenda/",
-  $cache_peer                           =  ".naturalis.nl/"
+  $cache_peer                           =  ".naturalis.nl/",
+  $@midori_path                         = "midori -i 300 -e Fullscreen -c /home/kiosk/.config/midori"
 )
 {
   include stdlib
