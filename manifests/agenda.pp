86c597372804c6ee39fa49ac709df3e6c02bff19
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-05-13 10:48:07 +0200

    removed unclutter

--- manifests/agenda.pp
+++ manifests/agenda.pp
@@ -13,7 +13,7 @@
 
 class kiosk::agenda(
   $mode                                 = "agenda",
-  $packages                             = ['xorg','openbox','squid3','unclutter'],
+  $packages                             = ['xorg','openbox','squid3'],
   $midoridirs                           = ['/home/kiosk/','/home/kiosk/.config','/home/kiosk/.config/midori','/home/kiosk/.config/midori/extensions','/home/kiosk/.config/midori/extensions/libmouse-gestures.so','/home/kiosk/.config/openbox','/home/kiosk/.local/','/home/kiosk/.local/share/','/home/kiosk/.local/share/midori','/home/kiosk/.local/share/midori/styles','/home/kiosk/.icons/','/home/kiosk/.icons/default/','/home/kiosk/.icons/default/cursors'],
   $midori_path                          = "midori -i 300 -e Fullscreen -c /home/kiosk/.config/midori",
   $local_proxy                          = "true",
