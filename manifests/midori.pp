3ca15bd89dc6ba67deaad380ef531844abc950b0
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-05-27 13:52:06 +0200

    transp cursor

--- manifests/midori.pp
+++ manifests/midori.pp
@@ -50,12 +50,19 @@ ensure_resource('file', '/etc/apt/sources.list.d',{
       require        => [Package[$packages]]
   }
 # configure transparent cursor
-  exec {"make_transparent":
-    command               => "/bin/sh configure && cd cursors && /usr/bin/make install-data-local DESTDIR=/home/kiosk/.icons/default CURSOR_DIR=/cursors",
+  exec {"config_transparent":
+    command               => "/bin/sh configure",
     cwd                   => "/tmp/xcursor-transparent-theme-0.1.1",
     unless                => "/usr/bin/test -d /home/kiosk/.icons/default/cursors/transp",
     require               => Exec["download_transparent"]
   }
+  # configure transparent cursor
+    exec {"make_transparent":
+      command               => "/usr/bin/make install-data-local DESTDIR=/home/kiosk/.icons/default CURSOR_DIR=/cursors",
+      cwd                   => "/tmp/xcursor-transparent-theme-0.1.1/cursors",
+      unless                => "/usr/bin/test -d /home/kiosk/.icons/default/cursors/transp",
+      require               => Exec["config_transparent"]
+    }
 # autoset transparent cursor
    file { '/home/kiosk/.icons/default/cursors/emptycursor':
     ensure                => present,
