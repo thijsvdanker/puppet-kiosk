5222b8c857ec09e3e98e520c74efdc717a95f202
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-06-05 11:43:12 +0200

    onlyif check

--- manifests/chromium.pp
+++ manifests/chromium.pp
@@ -111,7 +111,7 @@ ensure_resource('file', '/etc/apt/sources.list.d',{
 # set chromium config
 file_line { 'force_gpu':
   path                    => '/home/kiosk/.config/chromium/Local State',
-  ensure                  => present,
+  onlyif                  => '/home/kiosk/.config/chromium/Local State',
   line                    => 'enabled_labs_experiments": [  ],',
   match                   => 'enabled_labs_experiments": [ "ignore-gpu-blacklist" ],',
   require                 => [Package['chromium-browser'],File[$dirs]]
