d5d4ea31caad75ae7d8078cbe153f60a38cd0571
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-06-10 15:40:20 +0200

    chrome setup

--- manifests/chromium.pp
+++ manifests/chromium.pp
@@ -45,36 +45,27 @@ ensure_resource('file', '/etc/apt/sources.list.d',{
 #  }
 # install google-chrome
   file { "/etc/apt/sources.list.d/google.list":
-    owner => "root",
-    group => "root",
-    mode => 444,
-    source => "puppet:///modules/google-chrome/google.list",
-    notify => Exec["Google apt-key"],
+    owner                 => "kiosk",
+    group                 => "kiosk",
+    mode                  => 444,
+    content               => "deb http://dl.google.com/linux/deb/ stable main",
+    notify                => Exec["Google apt-key"],
   }
-
   # Add Google's apt-key.
-  # Assumes definition elsewhere of an Exec["apt-get update"] - or
-  # uncomment below.
   exec { "Google apt-key":
-    command => "/usr/bin/wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | /usr/bin/apt-key add -",
-    refreshonly => true,
-    notify => Exec["apt-get update"],
+    command               => "/usr/bin/wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | /usr/bin/apt-key add -",
+    refreshonly           => true,
+    notify                => Exec["apt-get update"],
   }
-
-  ## If not defined elsewhere, uncomment:
-   exec { "apt-get update":
-     command => "/usr/bin/apt-get update",
-     refreshonly => true,
-   }
-
-  # Install latest stable; remove beta first, if present:
-  package { "google-chrome-beta":
-    ensure => absent,
+  ## refresh:
+  exec { "apt-get update":
+    command               => "/usr/bin/apt-get update",
+    refreshonly           => true,
   }
-
+  # Install latest stable
   package { "google-chrome-stable":
-    ensure => latest, # to keep current with security updates
-    require => [ Exec["apt-get update"], Package["google-chrome-beta"], ],
+    ensure                => latest,
+    require               => [ Exec["apt-get update"]],
   }
 # download and untar transparent cursor
   exec { 'download_transparent':
