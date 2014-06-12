35ac033def1c37934577c54b75bc99210ad1d6ff
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-06-12 13:46:20 +0200

    added apache and php5

--- manifests/chrome.pp
+++ manifests/chrome.pp
@@ -186,7 +186,7 @@ ensure_resource('file', '/etc/apt/sources.list.d',{
     require               => [Package[$packages]]
     }
 # enable apache and php5 if needed
-  if ($enable_apache == "true") {
+  if $enable_apache == 'true' {
     $installed            = "present",
     $enable               = "true",
     $ensure               = "running"
